5335488e197e95f8944e72a5b2dd0db7c4441ed0
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-07 02:06:39 -0500

    adds validations for all user parameters in model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   has_many :shadows
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
