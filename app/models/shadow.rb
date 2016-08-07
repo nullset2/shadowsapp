0bb3e84fdaba0a5be7523caef383ab9aea2627e4
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-07 01:24:52 -0500

    adds shadow model and validations

--- app/models/shadow.rb
+++ app/models/shadow.rb
@@ -1,2 +1,8 @@
 class Shadow < ApplicationRecord
+  enum kind: [ :tree_shadow, :bus_stop, :building, :other ]
+  belongs_to :user
+
+  validates latitude: { presence: true }
+  validates longitude: { presence: true }
+  validates kind: { presence: true }
 end
