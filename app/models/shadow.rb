404295612825dd6c21e829899b29336f2cac007f
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-07 01:37:43 -0500

    fixes incorrect validations

--- app/models/shadow.rb
+++ app/models/shadow.rb
@@ -2,9 +2,9 @@ class Shadow < ApplicationRecord
   enum kind: [ :tree_shadow, :bus_stop, :building, :other ]
   belongs_to :user
 
-  validates latitude: { presence: true }
-  validates longitude: { presence: true }
-  validates kind: { presence: true }
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :kind, presence: true
 
   has_attached_file :photo, styles: { medium: "600x600>", thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
