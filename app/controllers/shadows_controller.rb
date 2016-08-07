2bb8633077abb528c5005be0f74a74a5c48de13b
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-08-07 02:43:54 -0500

    removes boilerplate on shadows controller

--- app/controllers/shadows_controller.rb
+++ app/controllers/shadows_controller.rb
@@ -6,48 +6,28 @@ class ShadowsController < ApplicationController
     @shadows = Shadow.all
   end
 
-  def show
-  end
-
-  def new
-    @shadow = Shadow.new
-  end
-
-  def edit
-  end
-
   def create
     @shadow = Shadow.new(shadow_params)
 
-    respond_to do |format|
       if @shadow.save
-        format.html { redirect_to @shadow, notice: 'Shadow was successfully created.' }
-        format.json { render :show, status: :created, location: @shadow }
+        render :show, status: :created, location: @shadow
       else
-        format.html { render :new }
-        format.json { render json: @shadow.errors, status: :unprocessable_entity }
+        render json: @shadow.errors, status: :unprocessable_entity
       end
     end
   end
 
   def update
-    respond_to do |format|
-      if @shadow.update(shadow_params)
-        format.html { redirect_to @shadow, notice: 'Shadow was successfully updated.' }
-        format.json { render :show, status: :ok, location: @shadow }
-      else
-        format.html { render :edit }
-        format.json { render json: @shadow.errors, status: :unprocessable_entity }
-      end
+    if @shadow.update(shadow_params)
+      render :show, status: :ok, location: @shadow
+    else
+      render json: @shadow.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     @shadow.destroy
-    respond_to do |format|
-      format.html { redirect_to shadows_url, notice: 'Shadow was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
